b01102f5693e12cac8393231bf5b0d144392f84c
Ye Fang Kuang <kuangyefang@live.ca>
2024-02-10 13:50:41 -0500

    comment on reference

--- src/components/auth/signup.jsx
+++ src/components/auth/signup.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import { Form, Button, Card } from 'react-bootstrap'
 
+//https://www.youtube.com/watch?v=PKwu15ldZ7k
+
 export default function signup() {
   return (
     <div>signup</div>
